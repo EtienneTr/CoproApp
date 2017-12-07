abc682cf8fc38656b785310a15c024fc28de2651
EtienneT <tienou42@gmail.com>
2017-12-07 15:30:48 +0100

    Entity User

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Etienne
+ * Date: 07/12/2017
+ * Time: 15:28
+ */
+
+namespace UserBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
