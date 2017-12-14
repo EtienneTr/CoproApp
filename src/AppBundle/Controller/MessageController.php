f70055f3430ef8f798874ce999b4e8cf5130fb44
EtienneT <tienou42@gmail.com>
2017-12-14 11:36:14 +0100

    import method

--- src/AppBundle/Controller/MessageController.php
+++ src/AppBundle/Controller/MessageController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class MessageController extends Controller
 {
