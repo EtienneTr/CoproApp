8b65e520beba053997597d12bcda5339c6086d31
TheGerbille <gerbilledu0138@gmail.com>
2018-01-11 17:14:33 +0100

    Ajout classe CSS

--- src/AppBundle/Resources/public/js/main/coproApp.js
+++ src/AppBundle/Resources/public/js/main/coproApp.js
@@ -1,1 +1,12 @@
-$('.selectpicker').parent().width('100%');
+var selectpicker = $('.selectpicker');
+selectpicker.parent().width('100%');
+var nothingSelectedText  = "";
+if(selectpicker.hasClass("select_other_than_user")){
+    nothingSelectedText = "Aucun élément sélectionné";
+}else{
+    nothingSelectedText = "Tout le monde";
+}
+
+selectpicker.selectpicker({
+    noneSelectedText : nothingSelectedText
+});
