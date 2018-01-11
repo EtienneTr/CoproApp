452e68b7e86e3fa63c0d604f859d27c9e4284887
TheGerbille <gerbilledu0138@gmail.com>
2018-01-11 08:27:32 +0100

    MAJ Notifications

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -2,11 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Service\NotificationService;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends Controller
 {
@@ -19,35 +18,4 @@ class DashboardController extends Controller
         return $this->render('AppBundle:dashboard:dashboard.html.twig', array());
     }
 
-    /**
-     * @Route("/send-notification", name="send_notification")
-     */
-    public function sendNotification(Request $request, NotificationService $manager)
-    {
-        $manager->createUserNotification("titile", "message", "fff");
-
-        return $this->redirectToRoute('dashboard');
-    }
-
-    /**
-     * List of all notifications
-     *
-     * @Route("/notification", name="notif_list")
-     * @Method("GET")
-     */
-    public function listAction()
-    {
-        $notifiableRepo = $this->get('doctrine.orm.entity_manager')->getRepository('MgiletNotificationBundle:NotifiableNotification');
-        $notifiable = $this->getUser()->getId();
-        $qb =  $notifiableRepo->createQueryBuilder('nn')
-            ->join('nn.notification', 'n')
-            ->join('nn.notifiableEntity', 'ne')
-            ->where('ne.identifier = :id')
-            ->setParameter('id', $notifiable)
-            ->getQuery()->getResult();
-
-        return $this->render('MgiletNotificationBundle::notifications.html.twig', array(
-            'notifiableNotifications' => $qb
-        ));
-    }
 }
---
+++ src/AppBundle/Controller/NotificationController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Service\NotificationService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class NotificationController extends Controller
+{
+    /**
+     * @Route("/send-notification", name="send_notification")
+     */
+    public function sendNotification(Request $request, NotificationService $manager)
+    {
+        $manager->createNotification("titile", "message", "fff");
+
+        return $this->redirectToRoute('dashboard');
+    }
+
+    /**
+     * List of all notifications
+     *
+     * @Route("/notification", name="notif_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $notifiableRepo = $this->get('doctrine.orm.entity_manager')->getRepository('MgiletNotificationBundle:NotifiableNotification');
+        $notifiable = $this->getUser()->getId();
+        $qb = $notifiableRepo->createQueryBuilder('nn')
+            ->join('nn.notification', 'n')
+            ->join('nn.notifiableEntity', 'ne')
+            ->where('ne.identifier = :id')
+            ->setParameter('id', $notifiable)
+            ->getQuery()->getResult();
+
+        return $this->render('AppBundle:notification:notification-list.html.twig', array(
+            'notifiableNotifications' => $qb
+        ));
+    }
+
+    /**
+     *
+     * @Route("notification/{notifiable}/mark_as_seen/{notification}", name="notif_mark_seen")
+     * @Method("POST")
+     *
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Doctrine\ORM\EntityNotFoundException
+     * @throws \LogicException
+     */
+    public function markasseenAction($notifiable, $notification)
+    {
+        $manager = $this->get('mgilet.notification');
+        $manager->markAsSeen(
+            $manager->getNotifiableInterface($manager->getNotifiableEntityById($notifiable)),
+            $manager->getNotification($notification),
+            true
+        );
+        return $this->redirectToRoute('notif_list');
+    }
+    /**
+     *
+     * @Route("notification/{notifiable}/mark_as_seen/{notification}", name="notif_mark_unseen")
+     * @Method("POST")
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Doctrine\ORM\EntityNotFoundException
+     * @throws \LogicException
+     */
+    public function markasunseenAction($notifiable, $notification)
+    {
+        $manager = $this->get('mgilet.notification');
+        $manager->markAsUnseen(
+            $manager->getNotifiableInterface($manager->getNotifiableEntityById($notifiable)),
+            $manager->getNotification($notification),
+            true
+        );
+        return $this->redirectToRoute('notif_list');
+    }
+}
